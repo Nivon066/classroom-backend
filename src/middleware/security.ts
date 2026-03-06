ff961cdeca160c43c986bd279899f5c36c1f5f9e
Aldo <rioszaldivaraldo@gmail.com>
2026-03-05 19:18:36 -0600

    implement class details

--- src/middleware/security.ts
+++ src/middleware/security.ts
@@ -13,19 +13,19 @@ const securityMiddelware = async (req: Request, res: Response, next: NextFunctio
 
         switch(role){
             case 'admin':
-                limit = 20;
+                limit = 500;
                 message = 'Admin request limit exceeded (20 per minute). Slow down';
                 break;
             case 'teacher':
-                limit = 2;
-                message = 'Admin request limit exceeded (20 per minute)';
+                limit = 500;
+                message = 'Teacher request limit exceeded (20 per minute)';
                 break;
             case 'student':
-                limit = 10;
+                limit = 500;
                 message = 'User request limit exceeded (10 per minute). Please wait';
                 break;
             default:
-                limit = 5;
+                limit = 500;
                 message = 'Guest limit exceeded (5 per minute). Please sign up for higher limits.';
                 break;
         }
--- src/routes/classes.ts
+++ src/routes/classes.ts
@@ -1,7 +1,7 @@
 import {and, desc, eq, getTableColumns, ilike, or, sql} from "drizzle-orm";
 import express from "express";
 import {db} from "../db/index.js";
-import {classes, subjects, user} from "../db/schema/index.js";
+import {classes, departments, subjects, user} from "../db/schema/index.js";
 const router = express.Router();
 
 // Get all classes with optional filtering and pagination
@@ -80,6 +80,35 @@ router.get("/", async (req, res) => {
         res.status(500).json({ error: 'Failed to get classes' });
     }
 });
+
+//Get class details with teacher, subject, and department
+router.get('/:id', async (req, res) => {
+    const classId = Number(req.params.id);
+    if(!Number.isFinite(classId)) return res.status(400).json({error: 'No class found'});
+
+    const [classDetails]= await db
+        .select({
+            ...getTableColumns(classes),
+            subject: {
+                ...getTableColumns(subjects)
+            },
+            department: {
+                ...getTableColumns(departments),
+            },
+            teacher: {
+                ...getTableColumns(user),
+            }
+
+        })
+        .from(classes)
+        .leftJoin(subjects, eq(classes.subjectId, subjects.id))
+        .leftJoin(user, eq(classes.teacherId, user.id))
+        .leftJoin(departments, eq(subjects.departmentId, departments.id))
+        .where(eq(classes.id, classId))
+
+    if(!classDetails) return res.status(404).json({error: 'No class found.'});
+    res.status(200).json({data: classDetails});
+})
 
 router.post("/", async (req, res) => {
     try{
